99eaaba35cba1cd1c765679a3ac48511902d130c
Agnes Wangechi <engwangechi@gmail.com>
2022-07-01 16:39:23 -0700

    Update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,13 @@ int main(void)
 	int x = '0';
 
 	while (x <= '9')
-	putchar(x);
-	if (x != '9')
-	putchar(',');
-	putchar(' ');
-	x++;
+	{
+		putchar(x);
+		if (x != '9')
+		putchar(',');
+		putchar(' ');
+		x++;
+	}
 	putchar(10);
 	return (0);
 }
