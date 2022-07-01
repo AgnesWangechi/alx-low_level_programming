d8bd509331bd622701e5563474b4be93c34c842d
Agnes Wangechi <engwangechi@gmail.com>
2022-07-01 14:13:02 -0700

    Update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+
 /**
  *main-
  *
  *Return: Always 0 (Success)
  */
 int main(void)
-{  
-  int num = '0';
-  
-  for (num = '0'; num <= 9; num++)
-  putchar(num);
-  putchar('\n');
-  return (0);
+{
+	int num = '0';
 
+	for (num = '0'; num <= 9; num++)
+	putchar(num);
+	putchar('\n');
+	return (0);
 }
