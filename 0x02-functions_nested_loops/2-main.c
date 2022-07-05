5b17aabcdff01b95d76042a2ed9609b9ac036297
Agnes Wangechi <engwangechi@gmail.com>
2022-07-05 12:11:58 -0700

    Create 2-alphabet_x10

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -7,15 +7,10 @@
 
 int main(void)
 {
+int i;
+for(i=0; i<=10; i++)
+{
 print_alphabet_x10();
-print_alphabet_x10();
-print_alphabet_x10();
-print_alphabet_x10();
-print_alphabet_x10();
-print_alphabet_x10();
-print_alphabet_x10();
-print_alphabet_x10();
-print_alphabet_x10();
-print_alphabet_x10();
+}
 return (0);
 }
