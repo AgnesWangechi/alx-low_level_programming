25d6f89b240a52fb3af502a295bfde41aeb8d1d6
AgnesWangechi <106992909+AgnesWangechi@users.noreply.github.com>
2022-07-08 02:14:19 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,10 +6,10 @@
  */
 void print_most_numbers(void)
 {
-int i = 1;
+int i = '1';
   
-for (i = 1; 1 <= 0; i++)
-if (i != 2 && ch != 4)
+for (i = '1'; 1 <= '0'; i++)
+if (i != '2' && ch != '4')
 _putchar(i);
 _putchar('\n');
 }
