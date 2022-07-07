90f70c1e9c6fd83315ec828f1c7b61bcccc0cf64
AgnesWangechi <106992909+AgnesWangechi@users.noreply.github.com>
2022-07-08 01:05:30 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,19 @@
+#include "main.h"
+/**
+ *_islower - function
+ *
+ * @c: The character to evaluate
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+int num = '0';
+
+for (num = '0'; num <= '9'; num++)
+_putchar(num);
+_putchar('\n');
+}
+
 
