453e4efce8b9719dd3263ccb6d5bc7b8c8bd9d5f
AgnesWangechi <106992909+AgnesWangechi@users.noreply.github.com>
2022-07-08 00:31:58 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,20 @@
-
+#include "main.h"
+/**
+ * _isdigit - function
+ *
+ * @c: The character to evaluate
+ *
+ * Return: Get 1 if c is uppercase,
+ * get 0 otherwise
+ */
+int _isdigit(int c)
+{
+if(c >=48 && c <=57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
