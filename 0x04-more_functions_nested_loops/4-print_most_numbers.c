a05ae005d11fd69d13e3d47df2b04f748acc599f
AgnesWangechi <106992909+AgnesWangechi@users.noreply.github.com>
2022-07-08 02:10:55 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,15 @@
-
+#include "main.h"
+/**
+ *print_alphabet - print alphabet in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+int i = 1;
+  
+for (i = 1; 1 <= 0; i++)
+if (i != 2 && ch != 4)
+_putchar(i);
+_putchar('\n');
+}
