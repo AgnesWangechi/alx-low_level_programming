d462417f7ad3c256bf116d37e365d75dcb7414c4
AgnesWangechi <106992909+AgnesWangechi@users.noreply.github.com>
2022-07-08 00:39:57 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,14 @@
-
+#include "main.h"
+/**
+ * _islower - function
+ *
+ * @c: The character to evaluate
+ *
+ * Return: Get 1 if c is uppercase,
+ * get 0 otherwise
+ */
+int mul(int a, int b)
+{
+multiply = a*b;
+return (multiply);
+}
