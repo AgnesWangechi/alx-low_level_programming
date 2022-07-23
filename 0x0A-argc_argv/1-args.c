27e0dc90eb46de8fd7fe14c995fde179fb6770e3
Agnes Wangechi <engwangechi@gmail.com>
2022-07-23 12:58:22 -0700

    Update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
  * @argv: array that contains the program comand line arguments
  * Return - Success 0
  */
-int main(int argc, char* argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-printf("%d\n", argc -1);
-return 0;
+printf("%d\n", argc - 1);
+return (0);
 }
