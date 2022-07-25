4816897edd4c24437b6ed1c41d8ab849f2efb20a
Agnes Wangechi <engwangechi@gmail.com>
2022-07-25 06:28:05 -0700

    Update

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * *_strdup(char *str) - function to allocate space in memory
+ * @*str: pointer holding str
+ * Return: Retrun the value
+ */
+
+char *_strdup(char *str)
+{
+int i, b;
+char str[] = "str";
+a = malloc(sizeof(str));
+a[0] = 's';
+if (str == NULL)
+{
+return (NULL);
+}
+else if (str != NULL)
+{
+while(b < a)
+{
+return (a);
+}
+}
+else
+{
+while(i > str)
+{
+a[i] == str;
+i++;
+}
+return (NULL);
+}
+}
