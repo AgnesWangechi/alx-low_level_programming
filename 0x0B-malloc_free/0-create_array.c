43fc17de00c3255c66856f4407411b3a15f598a4
Agnes Wangechi <engwangechi@gmail.com>
2022-07-25 05:50:43 -0700

    Update

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <std.lib>
+/**
+ * create_array - function to create an array and check if the array is empty
+ * @c: element of the array
+ * @size: parameter with size of array
+ * Return: A pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+char *p;
+unsigned int i = 0;
+
+p = malloc(sizeof(char) * size);
+if (size == 0)
+{
+return (NULL);
+}
+else if (p == NULL)
+{
+return (NULL);
+}
+else
+{
+while (i < size)
+{
+p[i] = c; /*assign address of char*/
+i++;
+}
+return (p);
+}
+}
