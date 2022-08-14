7cfa653d24773219ff2e1b319fa677264f13effc
Agnes Wangechi <engwangechi@gmail.com>
2022-08-14 05:08:59 -0700

    Create files

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,7 +10,7 @@ list_t *add_node(list_t **head, const char *str)
 list_t *newnode;
 newnode = malloc(sizeof(list_t));
 if (newnode == NULL)
-return (NULL);
+	return (NULL);
 newnode->str = strdup(str);
 newnode->len = _strlen(str);
 newnode->next = *head;
