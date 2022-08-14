d555137650bd9ddfe3d76c20d0e1f17c2ebd8c86
Agnes Wangechi <engwangechi@gmail.com>
2022-08-14 04:56:11 -0700

    Create files

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * add_node - add a new node at the beginning
+ * @head: double pointer to head
+ * @str: string to be saved in new node(duplicated)
+ * Return: Address of new element or NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *newnode;
+newnode = malloc(sizeof(list_t));
+if (newnode == NULL)
+return (NULL);
+newnode->str = strdup(str);
+newnode->len = _strlen(str);
+newnode->next = *head;
+*head = newnode;
+return (newnode);
+}
