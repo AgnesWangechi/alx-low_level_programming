f10fe5981bdc2569ec95fc63a537b73fddda5566
Agnes Wangechi <engwangechi@gmail.com>
2022-08-15 13:58:54 -0700

    Create file

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -12,8 +12,7 @@ typedef struct listint_s
 {
 int n;
 struct listint_s *next;
-} 
-listint_t;
+} listint_t;
 /**
  * struct listp_s - singly linked list
  * @p: pointers of nodes
@@ -24,8 +23,7 @@ typedef struct listp_s
 {
 void *p;
 struct listp_s *next;
-} 
-listp_t;
+} listp_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
